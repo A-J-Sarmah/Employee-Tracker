26e1f1f97dc46c271602a63781b9eecef9d490d3
A.J.Sarmah <thisisamlan49@gmail.com>
2021-08-01 09:14:14 +0530

    Installed React Redux Dependencies.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
+// import { createStore } from "redux";
+// import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// const store = createStore();
+
+ReactDOM.render(<App />, document.getElementById("root"));
